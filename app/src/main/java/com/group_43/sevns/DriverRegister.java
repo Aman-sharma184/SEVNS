2b0407cee4ad04d59eacc3eedccc57a4a97d18c1
Aman-sharma184 <amanshrma184@gmail.com>
2025-11-28 12:16:26 +0530

    Driver login/Registration added

---
+++ app/src/main/java/com/group_43/sevns/DriverRegister.java
@@ -0,0 +1,15 @@
+package com.group_43.sevns;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class DriverRegisterActivity implements DriverRegister {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_login);
+
+
+
+    
+}
